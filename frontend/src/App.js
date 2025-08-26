f4ed1851545733d5ea84e16f48bfcef8f6bf40f7
tprxhub <toyrxhub@thetoypharmacy.com>
2025-08-26 23:14:57 +0400

    Update App.js heading h1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -60,7 +60,8 @@ function App() {
 
   return (
     <div className="chat-container">
-      <h1>Attune™ by ToyRx</h1>
+      <h1>Troubleshoot with Attune™ by ToyRx</h1>
+      <h2>
       <div className="chat-box" ref={boxRef}>
         {messages.map((msg, idx) => (
           <div key={idx} className={`bubble ${msg.role}`}>
